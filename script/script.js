820829b4e17c3648d3253c0d8a8b9b6cf86f43c5
João Matheus <joaomatheus11@gmail.com>
2024-12-03 10:17:43 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -36,3 +36,82 @@ function mostrar() {
         button.innerHTML = 'Ver Mais'
     }
 }
+
+
+
+
+
+let btn = document.querySelectorAll('.btn')
+
+let icons = document.querySelectorAll('.icons')
+
+// Função criada para substituir o ícone, quando o cursor do mouse, está dentro do botão / link.
+
+function hoverOn() {
+    for (let c = 0; c < btn.length; c++) {
+        btn[c].addEventListener('mouseenter', () => {
+            if (btn[c] == btn[0]) {
+                icons[0].setAttribute('src', 'imagens/envelope-regular2.svg')
+            } else {
+                icons[1].setAttribute('src', 'imagens/discord-brands-solid2.svg')
+            } else {
+                icons[2].setAttribute('src', 'imagens/whatsapp-brands-solid2.svg')
+            }
+        })
+    }
+}
+
+hoverOn()
+
+
+// Função para desfazer a substituição dos ícones, quando o cursor do mouse estiver fora do botão / link.
+
+function hoverOff() {
+    for (let c = 0; c < btn.length; c++) {
+        btn[c].addEventListener('mouseleave', () => {
+            if (btn[c] == btn[0]) {
+                icons[0].setAttribute('src', 'imagens/envelope-regular.svg')
+            } else if (btn[c] == btn[1]) {
+                icons[1].setAttribute('src', 'imagens/discord-brands-solid.svg')
+            } else {
+                icons[2].setAttribute('src', 'imagens/whatsapp-brands-solid.svg')
+            }
+        })
+    }
+}
+
+hoverOff()
+
+
+
+const screenTouch = window.matchMedia("(max-width: 1007px)")
+
+function touchHover(event) {
+    if (event.matches) {
+        for (let c = 0; c < btn.length; c++) {
+            btn[c].addEventListener('touchstart', () => {
+                if (btn[c] == btn[0]) {
+                    icons[0].setAttribute('src', 'imagens/envelope-regular2.svg')
+                } else if (btn[c] == btn[1]) {
+                    icons[1].setAttribute('src', 'imagens/discord-brands-solid2.svg')
+                } else {
+                    icons[2].setAttribute('src', 'imagens/whatsapp-brands-solid2.svg')
+                }
+            })
+
+            btn[c].addEventListener('touchend', () => {
+                setTimeout(() => {
+                    if (btn[c] == btn[0]) {
+                        icons[0].setAttribute('src', 'imagens/envelope-regular.svg')
+                    } else if (btn[c] == btn[1]) {
+                        icons[1].setAttribute('src', 'imagens/discord-brands-solid.svg')
+                    } else {
+                        icons[2].setAttribute('src', 'imagens/whatsapp-brands-solid.svg')
+                    }
+                }, 3000)
+            })
+        }
+    }
+}
+
+touchHover(screenTouch)
